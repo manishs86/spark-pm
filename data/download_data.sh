f72d906754b05f0111c1f052fbc62fb5eb344474
musa-atlihan <msatlihan@gmail.com>
2020-10-24 17:34:19 +0300

    add the download script

---
+++ data/download_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://azuremlsampleexperiments.blob.core.windows.net/datasets/PdM_telemetry.csv \
+https://azuremlsampleexperiments.blob.core.windows.net/datasets/PdM_errors.csv \
+https://azuremlsampleexperiments.blob.core.windows.net/datasets/PdM_maint.csv \
+https://azuremlsampleexperiments.blob.core.windows.net/datasets/PdM_failures.csv \
+https://azuremlsampleexperiments.blob.core.windows.net/datasets/PdM_machines.csv
